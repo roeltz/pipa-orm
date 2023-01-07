5da01d1cf8e75c408ea4f3539d3e80df5d09ef96
Leonardo Rothe Tagliafico <leo_rothe@msn.com>
2023-01-06 19:19:04 -0500

    New HashTransform::applyFromDescriptor method for convenience in login scenarios

--- src/Pipa/ORM/Transform/HashTransform.php
+++ src/Pipa/ORM/Transform/HashTransform.php
@@ -1,9 +1,16 @@
 <?php
 
 namespace Pipa\ORM\Transform;
+
+use Pipa\ORM\Descriptor;
 use Pipa\ORM\Transform;
 
 class HashTransform implements Transform {
+
+	static function applyFromDescriptor($value, Descriptor $descriptor, $property) {
+		$hash = new self();
+		return $hash->apply($value, $descriptor->transformed[$property]["param"]);
+	}
 		
 	function apply($value, $param) {
 		if (preg_match('/^#/', $value)) {
